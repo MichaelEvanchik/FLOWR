0088a2373c586d529a9cdd6f4b3028b34a772264
Michael Evanchik <MichaelEvanchik@users.noreply.github.com>
2018-05-01 17:03:50 -0400

    Update xml_cte_update.sql

--- xml_cte_update.sql
+++ xml_cte_update.sql
@@ -4,27 +4,27 @@
 (
     SELECT *
           ,(
-				SELECT 
-				t.AdditionalInfo
-						.query(N'/Params/Account[(TenantFirstName/text())[1]=sql:column("FirstName") 
-											 and (TenantLastName/text())[1]=sql:column("LastName")]')
-						.query(N'<Account>
-								 {
-									for $nd in /Account/*
-									return 
-									if(local-name($nd)="TenantFirstName") then
-										<TenantFirstName>{concat("first"[1],xs:string(sql:column("se.id")))}</TenantFirstName>
-									else if(local-name($nd)="TenantLastName") then
-										<TenantLastName>{concat("last"[1],xs:string(sql:column("se.id")))}</TenantLastName>
-									else
-									$nd
-								 }  
-								 </Account> ') AS [*]
-				FROM #TempData AS t
-				INNER JOIN #SEAAccountExtended AS se ON t.RefID=se.SEAccountRefID
-				where t.id = ilv.id and t.AdditionalInfo.exist('/Params/Account/TenantFirstName/text()') = 1
-				ORDER BY se.id
-				FOR XML PATH(''),ROOT('Params'),TYPE
+	SELECT 
+	t.AdditionalInfo
+	.query(N'/Params/Account[(TenantFirstName/text())[1]=sql:column("FirstName") 
+	and (TenantLastName/text())[1]=sql:column("LastName")]')
+.query(N'<Account>
+	 {
+		for $nd in /Account/*
+		return 
+		if(local-name($nd)="TenantFirstName") then
+			<TenantFirstName>{concat("first"[1],xs:string(sql:column("se.id")))}</TenantFirstName>
+		else if(local-name($nd)="TenantLastName") then
+			<TenantLastName>{concat("last"[1],xs:string(sql:column("se.id")))}</TenantLastName>
+		else
+		$nd
+	 }  
+	 </Account> ') AS [*]
+	FROM #TempData AS t
+	INNER JOIN #SEAAccountExtended AS se ON t.RefID=se.SEAccountRefID
+	where t.id = ilv.id and t.AdditionalInfo.exist('/Params/Account/TenantFirstName/text()') = 1
+	ORDER BY se.id
+	FOR XML PATH(''),ROOT('Params'),TYPE
            ) AS NewAdditionalInfo
     FROM #TempData AS ilv
 )
@@ -42,25 +42,25 @@ update cte set AdditionalInfo = NewAdditionalInfo where not cte.NewAdditionalInf
 (
     SELECT *
           ,(
-				SELECT 
-				t.AdditionalInfo
-						.query(N'//Params[(TenantFirstName/text())[1]=sql:column("FirstName") 
-											 and (TenantLastName/text())[1]=sql:column("LastName")]')
-						.query(N'
-									for $nd in /Params/*
-									return 
-									if(local-name($nd)="TenantFirstName") then
-										<TenantFirstName>{concat("first"[1],xs:string(sql:column("se.id")))}</TenantFirstName>
-									else if(local-name($nd)="TenantLastName") then
-										<TenantLastName>{concat("last"[1],xs:string(sql:column("se.id")))}</TenantLastName>
-									else
-									$nd 
-								 ') AS [*]
-				FROM #TempData AS t
-				INNER JOIN #SEAAccountExtended AS se ON t.RefID=se.SEAccountRefID
-				where t.id = ilv.id and t.AdditionalInfo.exist('/Params/TenantFirstName/text()') = 1
-				ORDER BY se.id
-				FOR XML PATH(''),ROOT('Params'),TYPE
+	SELECT 
+	t.AdditionalInfo
+	.query(N'//Params[(TenantFirstName/text())[1]=sql:column("FirstName") 
+						 and (TenantLastName/text())[1]=sql:column("LastName")]')
+	.query(N'
+	for $nd in /Params/*
+	return 
+	if(local-name($nd)="TenantFirstName") then
+		<TenantFirstName>{concat("first"[1],xs:string(sql:column("se.id")))}</TenantFirstName>
+	else if(local-name($nd)="TenantLastName") then
+		<TenantLastName>{concat("last"[1],xs:string(sql:column("se.id")))}</TenantLastName>
+	else
+	$nd 
+	 ') AS [*]
+	FROM #TempData AS t
+	INNER JOIN #SEAAccountExtended AS se ON t.RefID=se.SEAccountRefID
+	where t.id = ilv.id and t.AdditionalInfo.exist('/Params/TenantFirstName/text()') = 1
+	ORDER BY se.id
+	FOR XML PATH(''),ROOT('Params'),TYPE
            ) AS NewAdditionalInfo
     FROM #TempData AS ilv
 )
@@ -77,27 +77,27 @@ update cte set AdditionalInfo = NewAdditionalInfo where not cte.NewAdditionalInf
 (
     SELECT *
           ,(
-				SELECT 
-				t.AdditionalInfo
-						.query(N'/AdditionalInfo/Params/Account[(TenantFirstName/text())[1]=sql:column("FirstName") 
-											 and (TenantLastName/text())[1]=sql:column("LastName")]')
-						.query(N'<Params><Account>
-								 {
-									for $nd in /Account/*
-									return 
-									if(local-name($nd)="TenantFirstName") then
-										<TenantFirstName>{concat("first"[1],xs:string(sql:column("se.id")))}</TenantFirstName>
-									else if(local-name($nd)="TenantLastName") then
-										<TenantLastName>{concat("last"[1],xs:string(sql:column("se.id")))}</TenantLastName>
-									else
-									$nd
-								 }  
-								 </Account></Params> ') AS [*]
-				FROM #TempData AS t
-				INNER JOIN #SEAAccountExtended AS se ON t.RefID=se.SEAccountRefID
-				where t.id = ilv.id and t.AdditionalInfo.exist('/AdditionalInfo/Params/Account/TenantFirstName/text()') = 1
-				ORDER BY se.id
-				FOR XML PATH(''),ROOT('AdditionalInfo'),TYPE
+	SELECT 
+	t.AdditionalInfo
+	.query(N'/AdditionalInfo/Params/Account[(TenantFirstName/text())[1]=sql:column("FirstName") 
+						 and (TenantLastName/text())[1]=sql:column("LastName")]')
+	.query(N'<Params><Account>
+			 {
+				for $nd in /Account/*
+				return 
+				if(local-name($nd)="TenantFirstName") then
+					<TenantFirstName>{concat("first"[1],xs:string(sql:column("se.id")))}</TenantFirstName>
+				else if(local-name($nd)="TenantLastName") then
+					<TenantLastName>{concat("last"[1],xs:string(sql:column("se.id")))}</TenantLastName>
+				else
+				$nd
+			 }  
+	 </Account></Params> ') AS [*]
+	FROM #TempData AS t
+	INNER JOIN #SEAAccountExtended AS se ON t.RefID=se.SEAccountRefID
+	where t.id = ilv.id and t.AdditionalInfo.exist('/AdditionalInfo/Params/Account/TenantFirstName/text()') = 1
+	ORDER BY se.id
+	FOR XML PATH(''),ROOT('AdditionalInfo'),TYPE
            ) AS NewAdditionalInfo
     FROM #TempData AS ilv
 )
